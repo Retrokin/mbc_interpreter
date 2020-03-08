d78e29b661132bf68b600328377dbc5c90e8ad07
Retrokin <kavenger45@gmail.com>
2020-03-08 13:10:15 -0600

    Core Complete
    -Added tableref variable type
    -Completed commands (FUNC, block commands, etc.)
    TODO: Test to see if it works and go from there.

--- mbc.lua
+++ mbc.lua
@@ -8,7 +8,7 @@ mbc_genvar={
 	function()					--TABLE
 		return {},0
 	end,
-	function(self,c,p)			--FUNCTION
+	function(c,p,self)			--FUNCTION
 	end,
 	function(c,p)			--NUMBER
 		return c[p]+c[p+1]*256+(c[p+2]+c[p+3]*256)/65536,4
@@ -32,6 +32,9 @@ mbc_genvar={
 	end,
 	function(c,p)			--INTERGER
 		return c[p]+c[p+1]*256,2
+	end,
+	function(c,p,self)		--TABLEREF
+		return self[c[p]],1
 	end
 }
 
@@ -41,9 +44,9 @@ mbc_runner=cwi:new{
 }
 
 function mbc_runner:init()
-	self.code,self.var,self.vark={},{},{}
+	self.code,self.var={},{}
 	for i=0,255 do
-		self.vars[i]={}
+		self.var[i]={}
 	end
 end
 
@@ -53,9 +56,9 @@ end
 
 function mbc_runner:getkey(t,id)
 	if t==1 then					--block variables
-		if #self.vars[254]>0 then return self.vars[0][254][#self.vars[0][254]]+id+1 end
+		return self.boff+id
 	elseif id>9 and id<100 then
-		for k,v in pairs(self.vars[t]) do
+		for k,v in pairs(self.var[t]) do
 			if sub(k,#k-1,#k)==id then return k end
 		end
 	end
@@ -63,7 +66,7 @@ function mbc_runner:getkey(t,id)
 end
 
 function mbc_runner:getvar(t,id)
-	return self.vars[t][self:getkey(t,id)]
+	return self.var[t][self:getkey(t,id)]
 end
 
 function mbc_runner:compile(c,p,l)
@@ -74,65 +77,118 @@ function mbc_runner:compile(c,p,l)
 end
 
 function mbc_runner:run(c)
-	self.jumps={}
-	c=self.code[c]
-	local p,jmp,a2,a3,p4=1,{}
+	c,self.boff,self.bptr=self.code[c],0,{}
+	local p,jmp=1,{}
 
 	local function _gv(p,t)
 		if not t then
 			t=p
 			p+=1
 		end
-		if mbc_genvar[t] then mbc_genvar[t](c,p,self)
-		else error("No variable type #" .. t .. "Line #" .. p) end
+		if mbc_genvar[t] then return mbc_genvar[t](c,p,self)
+		else error("no variable type #" .. t .. "; byte #" .. p) end
 	end
+	local _c1={ 			--a subset of commands for VAR
+		function(t,ks,v)		--SET
+			t[ks]=v
+		end,
+		function(t,ks,v)		--ADD
+			t[ks]=t[ks]+v
+		end,
+		function(t,ks,v)		--SUB
+			t[ks]=t[ks]-v
+		end,
+		function(t,ks,v)		--MUL
+			t[ks]=t[ks]*v
+		end,
+		function(t,ks,v)		--DIV
+			t[ks]=t[ks]/v
+		end,
+		function(t,ks,v)		--CONCAT
+			t[ks]=t[ks] .. v
+		end,
+		function(t,ks,v)		--IFEQUAL
+			t[ks]=t[ks]==v
+		end,
+		function(t,ks,v)		--IFLESS
+			t[ks]=t[ks]<v
+		end,
+		function(t,ks,v)		--IFLESSEQUAL
+			t[ks]=t[ks]<=v
+		end,
+		function(t,ks,v)		--SETTABLE
+			t=v
+		end
+	}
 	local _c={							--all code commands
 		function(self) 					--VAR
-			local v,ad,ns=_gv(p+4)
-			if a3==0 then t[ns]=v						--SET
-			elseif a3==1 then t[ns]+=v					--ADD
-			elseif a3==2 then t[ns]-=v					--SUB
-			elseif a3==3 then t[ns]*=v					--MUL
-			elseif a3==4 then t[ns]/=v					--DIV
-			elseif a3==5 then t[ns]=t[ns] .. v			--CONCAT
-			elseif a3==6 then t[ns]=t[ns]==v				--IFEQUAL
-			elseif a3==7 then t[ns]=t[ns]<v				--IFLESS
-			elseif a3==8 then t[ns]=t[ns]<=v			--IFLESSEQUAL
-			elseif a3==9 then t=v end 					--SETTABLE
+			local v,ad=_gv(p+4)
+			local t,ks=self.var[c[p+1]],self:getkey(c[p+1],c[p+2])
+			if _c1[c[p+3]] then _c1[c[p+3]](t,ks,v)
+			else error("unknown operation #" .. c[p+3] .. " for opcode var; byte #" .. p) end
 			return ad
 		end,
 		function(self)					--LUANAME
 			local s,ad=_gv(p+3,6)
-			t[a2]=s .. "_" .. c[a2]
+			t[c[p+2]]=s .. "_" .. c[c[p+2]]
 			return ad
 		end,
 		function(self)					--FUNC
-			local ad=_gv(p+1,1)
-			
-			return 3
+			local t,rk=_gv(p+1,1),self:getkey(c[p+3],c[p+4])
+			self.var[c[p+3]][rk](tbl_iunpack(self.var[255]))
+			return 4
+		end,
+		function()						--IF
+			if self:getvar(c[p+1],c[p+2]) then p=_gv(p+3,9)
+			else p=_gv(p+5,9) end
+			return 6
+		end,
+		function()				--BLOCKSTART
+			local v=_gv(p+1,8)
+			add(self.bptr,v)
+			self.boff+=v
+			return 1
+		end,
+		function()				--BLOCKEND
+			self.boff-=self.bptr[#self.bptr]
+			self.bptr[#self.bptr]=nil
+			return 0
+		end,
+		function()				--BLOCKFORWARD
+			local v=_gv(p+1,8)
+			self.boff+=self.bptr[#self.bptr-v]
+			return 1
+		end,
+		function()				--BLOCKBACK
+			local v=_gv(p+1,8)
+			self.boff-=self.bptr[#self.bptr-v]
+			return 1
 		end,
 		function()				--JUMPSTORE
 			add(jmp,p+1)
 			return 0
 		end,
 		function()				--JUMPBACK
-			if #jmp==0 then error("Nothing to jump back to! Line #" .. p) end
+			if #jmp==0 then error("nothing to jump back to!; byte #" .. p) end
 			p=self.jmp[#jmp]
 			jmp[#jmp]=nil
 			return 0
 		end,
 		function(self)				--JUMPTO
-			p=_gv(p+1,5)
+			p=_gv(p+1,9)
+			return 2
+		end,
+		function()					--DEBUGPRINT
+			printh("table #" c[p+1] .. " key["  .. self:getkey(p+1,p+2) .. "]=" .. tostr(_gv(p+1,1)))
 			return 2
 		end
 	}
-	
+
 	while p<=#c do
-		a2,a3,p4=c[p+2],c[p+3],p+4
-		local t=self.vars[c[p+1]]
+		local t=self.var[c[p+1]]
 		if _c[c[p]] then
 			local ad=_c[c[p]](self)
 			p+=ad+1
-		else error("Unknown command #" .. c[p]) end
+		else error("unknown opbyte #" .. c[p] .. "; byte #" .. p) end
 	end
 end
